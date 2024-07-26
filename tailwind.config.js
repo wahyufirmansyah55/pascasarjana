a0b5c9c42b1fde600ee189b457459630638412d6
wahyu firmansyah <whyfirmansyah69@gmail.com>
2024-07-26 20:45:15 +0700

    cimmit untuk pertama kali

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '32px',
+    },
+    extend: {
+      colors: {
+        primary: '#075985',
+        secondary: '#64748b',  
+        dark: '#0f172a',
+        orange: '#f58725',
+        dongker: '#202c6c',
+        orangemuda : '#fef9f3',
+      },
+      screens: {
+        '2xl': '1720px',
+      }
+    },
+  },
+  plugins: [],
+}
